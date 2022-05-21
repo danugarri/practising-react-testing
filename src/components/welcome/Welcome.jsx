7fcb7c7bd7bd89f46445436becb83fbd0acec41c
danugarri <danugarri@hotmail.com>
2022-05-21 13:52:05 +0200

    adding an aria-role attribute to the button switch  to select this specific button

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -13,7 +13,7 @@ export const Welcome = () => {
        { !nightMode && <h3>It is the white mode</h3>}
        { nightMode && <h3>It is the dark mode</h3>}
         <p>Click on the button bellow to choose your theme</p>
-        <button onClick = {handleNightMode}>Switch theme</button><hr/>
+        <button onClick = {handleNightMode} aria-label= 'switch'>Switch theme</button><hr/>
         <NavLink to ='/'>Home</NavLink>
     </div>
   )
--- src/components/welcome/Welcome.test.js
+++ src/components/welcome/Welcome.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import { Welcome } from "./Welcome";
 import {BrowserRouter as Router} from 'react-router-dom';
 
 describe('The Welcome component', () => { 
-    test('should check if the text A (It is the white mode) is in the document', () => {
+    test('should check if the text A (It is the white mode) is in the document without clicking the button', () => {
         // Arrange
         render(<Router><Welcome /></Router>);
         // ACT
@@ -11,6 +12,29 @@ describe('The Welcome component', () => {
         // Assetions
         const textA= screen.getByText('It is the white mode');
         expect(textA).toBeInTheDocument();
+
+    });
+     test('should check if the text A (It is the white mode) is in the document when clicking the button', () => {
+        // Arrange
+        render(<Router><Welcome /></Router>);
+        // ACT
+        const buttonElement= screen.getByRole('button', {name: 'switch'});
+        userEvent.click(buttonElement);
+        userEvent.click(buttonElement);
+        // Assetions
+        const textA= screen.getByText('It is the white mode', {exact:false});
+        expect(textA).toBeInTheDocument();
+
+    });
+     test('should check if the text B(It is the dark mode) is in the document after clicking the button', () => {
+        // Arrange
+        render(<Router><Welcome /></Router>);
+        // ACT
+        const buttonElement = screen.getByRole('button', {name: 'switch'});
+        userEvent.click(buttonElement);
+        // Assetions
+        const textB= screen.getByText('It is the dark mode');
+        expect(textB).toBeInTheDocument();
 
     });
  });
