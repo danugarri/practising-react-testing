37a441af0d23016a0f36c461eacf848dfe2ebb23
danugarri <danugarri@hotmail.com>
2022-05-21 15:32:44 +0200

    working context to pass the handleTheme logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { Context } from './components/context/Context';
+import { Context } from './components/context/Context';
 import { AsyncComponent } from './components/asynComponent/AsyncComponent';
 import { SnapshotComponent } from './components/snapshotComponent/SnapshotComponent';
 import { Welcome } from './components/welcome/Welcome';
@@ -7,21 +8,27 @@ import { Home } from './components/home/Home';
 
 
 function App() {
+  const [nightMode, setNightMode]= useState(false);
+  const handleNightMode= () => setNightMode(!nightMode);
+  const customTheme= nightMode ? 'white' : 'dark';
+
   return (
-    // <Context.Provider value={''}>
-    <Router>
-      <Routes>
-          <Route exact path="/" element={<Home />}>
-         </Route>
-         <Route path="snaphot" element={<SnapshotComponent />}>
-         </Route>
-         <Route path="welcome" element={<Welcome />}>
-         </Route>
-         <Route path="async" element={<AsyncComponent />}>
-         </Route>
-       </Routes>
-    </Router>
-    // </Context.Provider>
+    <Context.Provider value={{nightMode,handleNightMode}}>
+      <Router>
+          <div className={customTheme}> 
+        <Routes>
+            <Route exact path="/" element={<Home />}>
+            </Route>
+            <Route path="snaphot" element={<SnapshotComponent />}>
+            </Route>
+            <Route path="welcome" element={<Welcome />}>
+            </Route>
+            <Route path="async" element={<AsyncComponent />}>
+            </Route>
+        </Routes>
+          </div>
+      </Router>
+   </Context.Provider>
   
   );
 }
--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,20 +1,23 @@
-import React,{ useState} from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Context } from '../context/Context';
 import './Welcome.css';
 
 export const Welcome = () => {
-    const [nightMode, setNightMode]= useState(false);
-    const handleNightMode= () => setNightMode(!nightMode);
-    const customTheme= nightMode ? 'white' : 'dark'
-
+    
   return (
-    <div className= {customTheme}>
-        <p>Here you can change your UI from white to dark theme</p>
-       { !nightMode && <h3>It is the white mode</h3>}
-       { nightMode && <h3>It is the dark mode</h3>}
-        <p>Click on the button bellow to choose your theme</p>
-        <button onClick = {handleNightMode} aria-label= 'switch'>Switch theme</button><hr/>
-        <NavLink to ='/'>Home</NavLink>
-    </div>
+    <Context.Consumer>
+   {
+        ({nightMode,handleNightMode}) => (
+            <>
+            <p>Here you can change your UI from white to dark theme</p>
+            { !nightMode && <h3>It is the white mode</h3>}
+            { nightMode && <h3>It is the dark mode</h3>}
+                <p>Click on the button bellow to choose your theme</p>
+                <button onClick = {handleNightMode} aria-label= 'switch'>Switch theme</button><hr/>
+                <NavLink to ='/'>Home</NavLink>
+            </>
+        )}
+    </Context.Consumer>
   )
 }
