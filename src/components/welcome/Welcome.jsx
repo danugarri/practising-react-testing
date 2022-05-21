0ace37f41883462af1d1b4b4789a0dbb70b7d8c5
danugarri <danugarri@hotmail.com>
2022-05-21 15:57:11 +0200

    added better theme switch control

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Context } from '../context/Context';
-// import './Welcome.css';
 
 export const Welcome = () => {
     
