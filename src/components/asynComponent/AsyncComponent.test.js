4d7e8371c69e56667378fc35221aa965fa07ef37
danugarri <danugarri@hotmail.com>
2022-06-03 17:47:08 +0200

    added test mocking fetch function in order to make fake request

--- src/components/asynComponent/AsyncComponent.test.js
+++ src/components/asynComponent/AsyncComponent.test.js
@@ -1,4 +1,5 @@
 import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom'
 import { BrowserRouter } from 'react-router-dom';
 import { AsyncComponent } from './AsyncComponent';
 describe('The AsyncComponent', () => { 
@@ -13,5 +14,25 @@ describe('The AsyncComponent', () => {
         //  here we are storing an array of html elements
         expect(listItems).not.toHaveLength(0);
         // If this array is different from 0 that means the request return data
+    });
+});
+describe('The AsyncComponent ALSO', () => { 
+    it('should render the data if the request succeeds', async () => {
+        console.log('Here we are testing in a more precise way');
+        // Arrange
+        //  Replacing fetch() method with my own function
+        window.fetch= jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: (
+                async () => [{id:'test', title:'Esto es una prueba'}]
+            )
+        });
+        render(<BrowserRouter><AsyncComponent /></BrowserRouter>);
+        //  Act
+        // Asertion
+       
+        const listItems = await screen.findAllByRole('listitem');
+        expect(listItems).not.toHaveLength(0);
+       
     });
 });
